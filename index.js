d6bef51b816c13ba48f6f933ab6a1ea7d3e1d58b
Laevus92 <laevus@yandex.ru>
2023-02-24 09:01:55 +0300

    feat: add settings panel

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ let placeholderTranslations = {
     belorusian: ['[Увядзіце імя]', '[Увядзіце горад]', 'Минск'],
 }
 
+let settingsTranslation = {
+    english: ['Select language:', 'Select source of backgrounds:', 'Select Tag:', 'English', 'Russian', 'Belorussian', 'Nature', 'Space', 'Cities', 'Player', 'Weather', 'Time', 'Date', 'Greeting', 'Quote'],
+    russian: ['Выберите язык:', 'Выберите источник фонов:', 'Выберите тег:', 'Английский', 'Русский', 'Белорусский', 'Природа', 'Космос', 'Города', 'Плеер', 'Погода ', 'Время', 'Дата', 'Приветствие', 'Цитата'],
+    belorusian: ['Выберыце мову:', 'Выберыце крыніцу фонаў:', 'Выберыце тэг:', 'Англійская', 'Руская', 'Беларуская', 'Прырода', 'Космас', 'Гарады', 'Прайгравальнік', "Надвор'е", 'Час', 'Дата', 'Прывітанне', 'Цытата'] ,
+}
+
+
 function checkInput(currentLanguage) {
     if (userName.value !== ''){
         greetingWidth.textContent = `${userName.value}`;
@@ -63,66 +70,6 @@ function checkBlur(currentLanguage) {
 userName.addEventListener('blur', () => {
     checkBlur(currentLanguage);
 })
-
-function setLocalStorage() {
-    localStorage.setItem('name', userName.value);
-    localStorage.setItem('language', currentLanguage);
-    localStorage.setItem('width', greetingWidth.textContent);
-    localStorage.setItem('city', city.value);
-    localStorage.setItem('source', source);
-  }
-  window.addEventListener('beforeunload', setLocalStorage)
-
-  function getLocalStorage() {
-    if (localStorage.getItem('language')) {
-        currentLanguage = localStorage.getItem('language');
-        if (currentLanguage === 'russian') {
-            russianLanguage.classList.add('active');
-        } else if (currentLanguage === 'english') {
-            englishLanguage.classList.add('active');
-        } else { 
-            belorusianLanguage.classList.add('active');
-        }
-        checkInput(currentLanguage);
-        checkBlur(currentLanguage);
-        checkCity(currentLanguage);
-        getQuote(currentLanguage);
-    } else {
-        currentLanguage = 'english';
-        englishLanguage.classList.add('active')
-        checkInput(currentLanguage);
-        checkBlur(currentLanguage);
-        checkCity(currentLanguage);
-        getQuote(currentLanguage);
-    }
-
-    if(localStorage.getItem('name')) {
-        userName.value = localStorage.getItem('name');
-        greetingWidth.textContent = localStorage.getItem('width');
-        userName.style.width = `${greetingWidth.offsetWidth + 3}px`;
-    } else {
-        checkInput(currentLanguage);
-    }
-
-    if (localStorage.getItem('city')) {
-        city.value = localStorage.getItem('city');
-        getWeather(currentLanguage);
-    } else {
-        city.value = placeholderTranslations[currentLanguage][2];
-        getWeather(currentLanguage);
-    }
-
-    if (localStorage.getItem('source')) {
-        source = localStorage.getItem('source');
-        setBg();
-    } else {
-        source = 'github';
-        setBg();
-    }
-  }
-  window.addEventListener('load', () => {
-    getLocalStorage()
-  })  
 
 //creating watches
 
@@ -266,6 +213,7 @@ sourceFlickr.addEventListener('click', () => {
     sourceFlickr.classList.add('active');
     sourceUnsplash.classList.remove('active');
     tags.classList.add('active');
+    translateSettings()
     setBg();
 })
 
@@ -275,6 +223,7 @@ sourceUnsplash.addEventListener('click', () => {
     sourceFlickr.classList.remove('active');
     sourceUnsplash.classList.add('active');
     tags.classList.add('active');
+    translateSettings()
     setBg();
 })
  
@@ -282,7 +231,7 @@ sourceUnsplash.addEventListener('click', () => {
 async function getImage() {
     if (source === 'unsplash') {
         if (tag === '') {
-            tag = timeOfDay ()
+            tag = timeOfDay()
         }
         const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=${tag}&client_id=4bBeJ7mog7Gh_zh8eb0eveTwetEVzARpyyLUwpZd0ec`;
         const res = await fetch(url);
@@ -295,12 +244,11 @@ async function getImage() {
         } 
     } else if (source === 'flickr') {
         if (tag === '') {
-            tag = timeOfDay ()
+            tag = timeOfDay()
         }
         const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=074f354c06cc01cd38a3c9c758e1e7d0&tags=${tag}&extras=url_l&format=json&nojsoncallback=1`;
         const res = await fetch(url);
         const data = await res.json();
-        console.log (url)
         const img = new Image();
         img.src = `${data.photos.photo[getRandomNum(0, 99)].url_l}`; 
         img.onload = () => {      
@@ -647,34 +595,69 @@ track.forEach(track => {
 const englishLanguage = document.querySelector('.button.lang_eng');
 const russianLanguage = document.querySelector('.button.lang_rus');
 const belorusianLanguage = document.querySelector('.button.lang_bel');
+function translateSettings() {
+    if (Array.from(tags.classList).includes('active')) {
+        document.querySelector("body > div > div.settings.language > div.title").textContent = settingsTranslation[currentLanguage][0];
+        document.querySelector("body > div > div.settings.background__sources > div.title").textContent = settingsTranslation[currentLanguage][1];
+        document.querySelector("body > div > div.settings.tags.active > div.title").textContent = settingsTranslation[currentLanguage][2];
+        englishLanguage.textContent = settingsTranslation[currentLanguage][3];
+        russianLanguage.textContent = settingsTranslation[currentLanguage][4];
+        belorusianLanguage.textContent = settingsTranslation[currentLanguage][5];
+        natureTag.textContent = settingsTranslation[currentLanguage][6];
+        spaceTag.textContent = settingsTranslation[currentLanguage][7];
+        cityTag.textContent = settingsTranslation[currentLanguage][8];
+        widgetPlayer.textContent = settingsTranslation[currentLanguage][9];
+        widgetWeather.textContent = settingsTranslation[currentLanguage][10];
+        widgetTime.textContent = settingsTranslation[currentLanguage][11];
+        widgetDate.textContent = settingsTranslation[currentLanguage][12];
+        widgetGreeting.textContent = settingsTranslation[currentLanguage][13];
+        widgetQuote.textContent = settingsTranslation[currentLanguage][14];
+    } else {
+        document.querySelector("body > div > div.settings.language > div.title").textContent = settingsTranslation[currentLanguage][0];
+        document.querySelector("body > div > div.settings.background__sources > div.title").textContent = settingsTranslation[currentLanguage][1];
+        englishLanguage.textContent = settingsTranslation[currentLanguage][3];
+        russianLanguage.textContent = settingsTranslation[currentLanguage][4];
+        belorusianLanguage.textContent = settingsTranslation[currentLanguage][5];
+        natureTag.textContent = settingsTranslation[currentLanguage][6];
+        spaceTag.textContent = settingsTranslation[currentLanguage][7];
+        cityTag.textContent = settingsTranslation[currentLanguage][8];
+        widgetPlayer.textContent = settingsTranslation[currentLanguage][9];
+        widgetWeather.textContent = settingsTranslation[currentLanguage][10];
+        widgetTime.textContent = settingsTranslation[currentLanguage][11];
+        widgetDate.textContent = settingsTranslation[currentLanguage][12];
+        widgetGreeting.textContent = settingsTranslation[currentLanguage][13];
+        widgetQuote.textContent = settingsTranslation[currentLanguage][14];
+    }
+}
 
-
-
-function translateToEnglish () {
+function translateToEnglish() {
     currentLanguage = 'english';
     checkInput(currentLanguage);
     checkBlur(currentLanguage);
     checkCity(currentLanguage);
     getWeather(currentLanguage);
     getQuote(currentLanguage);
+    translateSettings()
 }
 
-function translateToRussian () {
+function translateToRussian() {
     currentLanguage = 'russian';
     checkInput(currentLanguage);
     checkBlur(currentLanguage);
     checkCity(currentLanguage);
     getWeather(currentLanguage);
     getQuote(currentLanguage);
+    translateSettings()
 }
 
-function translateToBelorussian () {
+function translateToBelorussian() {
     currentLanguage = 'belorusian';
     checkInput(currentLanguage);
     checkBlur(currentLanguage);
     checkCity(currentLanguage);
     getWeather(currentLanguage);
     getQuote(currentLanguage);
+    translateSettings()
 }
 
 englishLanguage.addEventListener('click', () => {
@@ -703,3 +686,277 @@ belorusianLanguage.addEventListener('click', () => {
         translateToBelorussian()
     }
 });
+
+const widgetPlayer = document.querySelector('.button.player');
+const widgetWeather = document.querySelector('.button.weather');
+const widgetTime = document.querySelector('.button.time');
+const widgetDate = document.querySelector('.button.date');
+const widgetGreeting = document.querySelector('.button.greeting');
+const widgetQuote = document.querySelector('.button.quote');
+let playerVisibility = 'visible';
+let weatherVisibility = 'visible';
+let timeVisibility = 'visible';
+let dateVisibility = 'visible';
+let greetingVisibility = 'visible';
+let quoteVisibility = 'visible';
+
+function hidePlayer() {
+    if (playerVisibility !== 'visible') {
+        document.querySelector("body > header > div.player").classList.add('hidden');
+        widgetPlayer.classList.add('active');
+    } else {
+        document.querySelector("body > header > div.player").classList.remove('hidden');
+        widgetPlayer.classList.remove('active');
+    }
+}
+
+function hideWeather() {
+    if (weatherVisibility !== 'visible') {
+        document.querySelector("body > header > div.weather").classList.add('hidden');
+        widgetWeather.classList.add('active');
+    } else {
+        document.querySelector("body > header > div.weather").classList.remove('hidden');
+        widgetWeather.classList.remove('active');
+    }
+}
+
+function hideTime() {
+    if (timeVisibility !== 'visible') {
+        document.querySelector('.current__time').classList.add('hidden');
+        widgetTime.classList.add('active')
+    } else {
+        document.querySelector('.current__time').classList.remove('hidden');
+        widgetTime.classList.remove('active')
+    }
+}
+
+function hideDate() {
+    if (dateVisibility !== 'visible') {
+        document.querySelector('.current__data').classList.add('hidden');
+        widgetDate.classList.add('active')
+    } else {
+        document.querySelector('.current__data').classList.remove('hidden');
+        widgetDate.classList.remove('active')
+    }
+}
+
+function hideGreeting() {
+    if (greetingVisibility !== 'visible') {
+        document.querySelector("body > main > div.time__calendar__greetings > div.greeting").classList.add('hidden');
+        widgetGreeting.classList.add('active')
+    } else {
+        document.querySelector("body > main > div.time__calendar__greetings > div.greeting").classList.remove('hidden');
+        widgetGreeting.classList.remove('active')
+    }
+}
+
+function hideQuote() {
+    if (quoteVisibility !== 'visible') {
+        document.querySelector('.wrapper').classList.add('hidden');
+        widgetQuote.classList.add('active')
+    } else {
+        document.querySelector('.wrapper').classList.remove('hidden');
+        widgetQuote.classList.remove('active')
+    }
+}
+
+widgetPlayer.addEventListener('click', () => {
+    if (playerVisibility === 'visible') {
+        playerVisibility = 'hide';
+    } else {
+        playerVisibility = 'visible';
+    }
+    hidePlayer()
+})
+
+widgetWeather.addEventListener('click', () => {
+    if (weatherVisibility === 'visible') {
+        weatherVisibility = 'hide';
+    } else {
+        weatherVisibility = 'visible';
+    }
+    hideWeather();
+})
+
+widgetTime.addEventListener('click', () => {
+    if (timeVisibility === 'visible') {
+        timeVisibility = 'hide';
+    } else {
+        timeVisibility = 'visible';
+    }
+    hideTime()
+})
+
+widgetDate.addEventListener('click', () => {
+    if (dateVisibility === 'visible') {
+        dateVisibility = 'hide';
+    } else {
+        dateVisibility = 'visible';
+    }
+    hideDate()
+})
+
+widgetGreeting.addEventListener('click', () => {
+    if (greetingVisibility == 'visible') {
+        greetingVisibility = 'hide';
+    } else {
+        greetingVisibility = 'visible';
+    }
+    hideGreeting()
+})
+
+widgetQuote.addEventListener('click', () => {
+    if (quoteVisibility == 'visible') {
+        quoteVisibility = 'hide';
+    } else {
+        quoteVisibility = 'visible';
+    }
+    hideQuote()
+})
+
+function setLocalStorage() {
+    localStorage.setItem('name', userName.value);
+    localStorage.setItem('language', currentLanguage);
+    localStorage.setItem('width', greetingWidth.textContent);
+    localStorage.setItem('city', city.value);
+    localStorage.setItem('source', source);
+    localStorage.setItem('tag', tag);
+    localStorage.setItem('playerVisibility', playerVisibility);
+    localStorage.setItem('weatherVisibility', weatherVisibility);
+    localStorage.setItem('timeVisibility', timeVisibility);
+    localStorage.setItem('dateVisibility', dateVisibility);
+    localStorage.setItem('greetingVisibility', greetingVisibility);
+    localStorage.setItem('quoteVisibility', quoteVisibility);
+  }
+
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+    
+    if (localStorage.getItem('tag') ) {
+        tag = localStorage.getItem('tag');
+        if (tag === 'nature') {
+            natureTag.classList.add('active');
+        } else if (tag === 'space') {
+            spaceTag.classList.add('active');
+        } else {
+            cityTag.classList.add('active');
+        }
+    } else {
+        tag = ''
+    }
+
+    if (localStorage.getItem('source')) {
+        source = localStorage.getItem('source');
+        if (source ==='unsplash') {
+            sourceUnsplash.classList.add ('active');
+            tags.classList.add('active');
+            document.querySelector("body > div > div.settings.tags.active > div.title").textContent = settingsTranslation[currentLanguage][2];
+        } else if (source ==='flickr') {
+            sourceFlickr.classList.add ('active');
+            tags.classList.add('active');
+            document.querySelector("body > div > div.settings.tags.active > div.title").textContent = settingsTranslation[currentLanguage][2];
+        } else {
+            sourceGithub.classList.add('active');
+            tags.classList.remove('active');
+        }
+        setBg();
+    } else {
+        source = 'github';
+        sourceGithub.classList.add('active');
+        tags.classList.remove('active');
+        setBg();
+    }
+
+    if (localStorage.getItem('language')) {
+        currentLanguage = localStorage.getItem('language');
+        if (currentLanguage === 'russian') {
+            russianLanguage.classList.add('active');
+            translateSettings()
+        } else if (currentLanguage === 'english') {
+            englishLanguage.classList.add('active');
+            translateSettings()
+        } else { 
+            belorusianLanguage.classList.add('active');
+            translateSettings()
+        }
+        checkInput(currentLanguage);
+        checkBlur(currentLanguage);
+        checkCity(currentLanguage);
+        getQuote(currentLanguage);
+    } else {
+        currentLanguage = 'english';
+        englishLanguage.classList.add('active')
+        translateSettings()
+    }
+
+    if(localStorage.getItem('name')) {
+        userName.value = localStorage.getItem('name');
+        greetingWidth.textContent = localStorage.getItem('width');
+        userName.style.width = `${greetingWidth.offsetWidth + 3}px`;
+    } else {
+        checkInput(currentLanguage);
+    }
+
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+        getWeather(currentLanguage);
+    } else {
+        city.value = placeholderTranslations[currentLanguage][2];
+        getWeather(currentLanguage);
+    }
+
+    if (localStorage.getItem('playerVisibility')) {
+        playerVisibility = localStorage.getItem('playerVisibility');
+        hidePlayer()
+    } else {
+        playerVisibility = 'visible';
+        hidePlayer()
+    }
+
+    if (localStorage.getItem('weatherVisibility')) {
+        weatherVisibility = localStorage.getItem('weatherVisibility');
+        hideWeather()
+    } else {
+        weatherVisibility = 'visible';
+        hideWeather()
+    }
+
+    if (localStorage.getItem('timeVisibility')) {
+        timeVisibility = localStorage.getItem('timeVisibility');
+        hideTime()
+    } else {
+        timeVisibility = 'visible';
+        hideTime()
+    }
+
+    if (localStorage.getItem('dataVisibility')) {
+        dateVisibility = localStorage.getItem('dataVisibility');
+        hideDate()
+    } else {
+        dateVisibility = 'visible';
+        hideDate()
+    }
+
+    if (localStorage.getItem('greetingVisibility')) {
+        greetingVisibility = localStorage.getItem('greetingVisibility');
+        hideGreeting()
+    } else {
+        greetingVisibility = 'visible';
+        hideGreeting()
+    }
+
+    if (localStorage.getItem('quoteVisibility')) {
+        quoteVisibility = localStorage.getItem('quoteVisibility');
+        hideQuote()
+    } else {
+        quoteVisibility = 'visible';
+        hideQuote()
+    }
+  }
+  window.addEventListener('load', () => {
+    setTimeout( () => {
+        document.getElementsByTagName("body")[0].style.opacity = "1";
+    }, 700);
+    getLocalStorage()
+}) 
